f721018d44699f0a00ccf6124562f7dd5703514d
arjantijms <arjan.tijms@gmail.com>
2018-08-21 15:59:32 +0200

    Initial project setup

---
+++ test-utils/src/main/java/org/eclipse/microprofile14/Libraries.java
@@ -0,0 +1,16 @@
+package org.eclipse.microprofile14;
+
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+
+public class Libraries {
+    
+    public static JavaArchive[] awaitability() {
+        return Maven.resolver()
+                .loadPomFromFile("pom.xml")
+                .resolve("com.jayway.awaitility:awaitility")
+                .withTransitivity()
+                .as(JavaArchive.class);
+    }
+
+}
---
+++ test-utils/src/main/java/org/eclipse/microprofile14/Parameter.java
@@ -0,0 +1,12 @@
+package org.eclipse.microprofile14;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+public @interface Parameter {
+}
+
